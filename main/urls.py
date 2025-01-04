4e329af794ec0b91106df5bcf39ba80883ed0a2a
Andris Jancevskis <andris.jancevskis@gmail.com>
2025-01-04 04:04:59 +0000

    Add about page

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import index
+from .views import index, about
 
 """
 Main URL patterns
 """
 urlpatterns = [
     path('', index, name='home'),
+    path('about/', about, name='about'),
 ]
 
